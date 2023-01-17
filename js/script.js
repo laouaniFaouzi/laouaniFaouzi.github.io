201f1e54c796e2ef22dfbf2822ea0ce42d76d578
laouaniFaouzi <76941273+laouaniFaouzi@users.noreply.github.com>
2023-01-18 00:46:06 +0100

    Fix js code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
-const currentTime = new Date();
-const currentHour = currentTime.getHours();
-
 function isMorning() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
     if (currentHour >= 6 && currentHour < 12) {
       return true;
     }
     return false;
-}
-function isNoon() {
+  }
+  
+  function isNoon() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
     if (currentHour >= 12 && currentHour < 14) {
       return true;
     }
     return false;
-}
-function isAfternoon() {
+  }
+  
+  function isAfternoon() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
     if (currentHour >= 14 && currentHour < 18) {
       return true;
     }
     return false;
-}
-function isEvening() {
+  }
+  
+  function isEvening() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
     if (currentHour >= 18 && currentHour < 23) {
       return true;
     }
     return false;
-}
-function isNight() {
+  }
+  
+  function isNight() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
     if (currentHour >= 23 && currentHour < 6) {
       return true;
     }
     return false;
-}
+  }
 
 if (isMorning()) {
     console.log("Morniiiing!");
