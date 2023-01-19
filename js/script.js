1d735e7fa0055af08de81f85d0eab84232cae8cd
laouaniFaouzi <76941273+laouaniFaouzi@users.noreply.github.com>
2023-01-19 01:10:48 +0100

    New js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+// Récupère les différentes périodes de la journée
 function getTimeOfDay() {
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
-    if (currentHour >= 6 && currentHour < 12) {
-      return "morning";
-    } else if (currentHour >= 12 && currentHour < 14) {
+    if (currentHour >= 6 && currentHour < 10) {
+      return "earlyMorning";
+    } else if (currentHour >= 10 && currentHour < 12) {
+        return "morning";
+    } else if (currentHour >= 12 && currentHour < 13) {
       return "noon";
-    } else if (currentHour >= 14 && currentHour < 18) {
+    } else if (currentHour >= 13 && currentHour < 18) {
       return "afternoon";
     } else if (currentHour >= 18 && currentHour < 23) {
       return "evening";
@@ -13,17 +16,20 @@ function getTimeOfDay() {
       return "night";
     }
 }
-  
+
+const timeOfDayDiv = document.getElementById("time-of-day");
 const time = getTimeOfDay();
 
-if (time === "morning") {
-    console.log("Morniiiing!");
+if (time === "earlyMorning") {
+    timeOfDayDiv.innerHTML = "Bonjour ! Réveille-toi et sent l'odeur du café, ou reste endormi et sent l'odeur de l'échec.";
+} else if (time === "morning") {
+    timeOfDayDiv.innerHTML = "Bonjour ! Le matin est comme un cadeau, on ne sait jamais à quoi s'attendre, sauf que c'est généralement du café.";
 } else if (time === "noon") {
-    console.log("Enjoy your meal!");
+    timeOfDayDiv.innerHTML = "Bon appétit ! Mange comme si ta vie en dépendait, parce que tu sais bien que c'est le cas.";
 } else if (time === "afternoon") {
-    console.log("Hi there !");
+    timeOfDayDiv.innerHTML = "Bonjour ! On dirait que c'est l'après-midi, temps idéal pour faire une pause, prendre un café et se rappeler pourquoi on a détesté le matin.";
 } else if (time === "evening") {
-    console.log("it's dinner tiiiiiime!");
+    timeOfDayDiv.innerHTML = "Bonsoir ! C'est l'heure de mettre de côté les soucis de la journée et de s'occuper de l'essentiel : manger des pâtes et regarder des films où encore regarder ma page.";
 } else if (time === "night") {
-    console.log("You are a late sleeper!");
+    timeOfDayDiv.innerHTML = "Bonsoir ! Il est tard, il est temps de mettre fin à cette journée et de laisser les rêves prendre le relais, ou de continuer à scroll sur ma page jusqu'à l'aube.";
 }
