beafe1cbe3b0cf8cdbb8e2f6b2f0d0c7d52291f4
laouaniFaouzi <76941273+laouaniFaouzi@users.noreply.github.com>
2023-02-26 02:43:49 +0100

    New adding

--- js/mini-apps.js
+++ js/mini-apps.js
@@ -4,7 +4,7 @@ appLinkButton.addEventListener("click", function () {
     var password = window.prompt("Entrez le mot de passe :"); // affiche une boîte de dialogue demandant le mot de passe
 
     if (password === "Lydia") {
-        window.location.href = "lydia-com-calculator.html"; // redirige vers la page protégée
+        window.location.href = "https://laouanifaouzi.github.io/mini-apps.html"; // redirige vers la page protégée
     } else {
         alert("Mot de passe incorrect. Veuillez réessayer."); // affiche une alerte si le mot de passe est incorrect
     }
