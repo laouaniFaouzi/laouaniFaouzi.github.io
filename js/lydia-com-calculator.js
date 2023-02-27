35d44450aaa095f8d2e8097d6a3518ddac809e8c
laouaniFaouzi <76941273+laouaniFaouzi@users.noreply.github.com>
2023-02-27 11:44:10 +0100

    New update

--- js/lydia-com-calculator.js
+++ js/lydia-com-calculator.js
@@ -3,6 +3,9 @@ const table = document.getElementById("employee-table");
 const list = document.getElementById("employee-list");
 const totalCommissionDiv = document.getElementById("total-commission");
 const totalSalesDiv = document.getElementById("total-sales");
+const totalSalaryDiv = document.getElementById("total-salary");
+const totalKpisDiv = document.getElementById("total-kpis");
+const totalConsultantDiv = document.getElementById("total-consultant");
 
 let totalCommission = 0;
 let totalSales = 0;
@@ -13,24 +16,55 @@ form.addEventListener("submit", (event) => {
   const name = form.elements.name.value;
   const position = form.elements.position.value;
   const sales = form.elements.sales.value;
+  const kpis = form.elements.kpis.value;
+  const seniority = form.elements.seniority.value;
 
   let commission = 0;
   let presence = 0;
-  if (position === "junior") {
+  if (position === "Junior") {
     commission = 50;
     presence = 50;
-  } else if (position === "senior") {
+  } else if (position === "Senior") {
     commission = 100;
     presence = 100;
+  } else if (position === "Team leader") {
+    commission = 500;
+    presence = 500;
+  } else if (position === "Manager") {
+    commission = 1000;
+    presence = 1000;
   }
 
-  if ( sales >= 15000 ) {
+  let seniorityValue = 0;
+  if (seniority === "6-12 mounth") {
+    seniorityValue = 100;
+  } else if (seniority === "12-18 mounth") {
+    seniorityValue = 150;
+  } else if (seniority === "18-24 mounth") {
+    seniorityValue = 200;
+  } else if (seniority === "+24 mounth") {
+    seniorityValue = 250;
+  }
+
+  let kpisValue = 0;
+  let kpisAmount = 0;
+  if (kpis === "Yes") {
+    kpisValue = 200;
+    kpisAmount = 200;
+  }
+
+  if (sales >= 20000) {
     commission += parseFloat((sales * 0.05).toFixed(2));
   }
+
+  commission += (kpisValue += seniorityValue);
 
   totalSales += parseFloat(sales);
 
   totalCommission += commission;
+
+  let totalSalary = 6000;
+  totalSalary += totalCommission;
 
   const row = document.createElement("tr");
 
@@ -41,6 +75,14 @@ form.addEventListener("submit", (event) => {
   const positionCell = document.createElement("td");
   positionCell.textContent = position + " (+" + presence + "€)";
   row.appendChild(positionCell);
+
+  const seniorityCell = document.createElement("td");
+  seniorityCell.textContent = seniority + " (+" + seniorityValue + "€)";
+  row.appendChild(seniorityCell);
+
+  const kpisCell = document.createElement("td");
+  kpisCell.textContent = kpis + " (+" + kpisAmount + "€)";
+  row.appendChild(kpisCell);
 
   const salesCell = document.createElement("td");
   salesCell.textContent = sales + "€";
@@ -57,9 +99,14 @@ form.addEventListener("submit", (event) => {
   deleteButton.addEventListener("click", () => {
     row.remove();
     totalCommission -= commission;
+    totalSales -= sales;
     totalCommissionDiv.textContent = totalCommission.toFixed(2) + "€";
+    totalSalesDiv.textContent = totalSales.toFixed(2) + "€";
+    totalSalaryDiv.textContent = totalSalary.toFixed(2) + "€";
+    totalKpisDiv.textContent = totalKpis;
+    totalConsultantDiv.textContent = totalConsultant;
   });
-  
+
   const deleteCell = document.createElement("td");
   deleteCell.appendChild(deleteButton);
   row.appendChild(deleteCell);
@@ -67,23 +114,26 @@ form.addEventListener("submit", (event) => {
 
   totalCommissionDiv.textContent = totalCommission.toFixed(2) + "€";
   totalSalesDiv.textContent = totalSales.toFixed(2) + "€";
+  totalSalaryDiv.textContent = totalSalary.toFixed(2) + "€";
+  totalKpisDiv.textContent = totalKpis;
+  totalConsultantDiv.textContent = totalConsultant;
 
 });
 
-document.querySelector("#download-pdf").addEventListener("click", function() {
-    // Code pour générer et enregistrer le PDF
-    var doc = new jsPDF('p', 'pt', 'letter');
-    var employeeTable = document.querySelector("#employee-table");
-    var res = doc.autoTableHtmlToJson(employeeTable);
-    doc.autoTable(res.columns, res.data, {
-      startY: 60,
-      theme: 'grid',
-      styles: {
-        overflow: 'linebreak',
-        fontSize: 10,
-        cellPadding: 5,
-        halign: 'center'
-      }
-    });
-    doc.save('employee-table.pdf');
+document.querySelector("#download-pdf").addEventListener("click", function () {
+  // Code pour générer et enregistrer le PDF
+  var doc = new jsPDF('p', 'pt', 'letter');
+  var employeeTable = document.querySelector("#employee-table");
+  var res = doc.autoTableHtmlToJson(employeeTable);
+  doc.autoTable(res.columns, res.data, {
+    startY: 60,
+    theme: 'grid',
+    styles: {
+      overflow: 'linebreak',
+      fontSize: 10,
+      cellPadding: 5,
+      halign: 'center'
+    }
   });
+  doc.save('employee-table.pdf');
+});
