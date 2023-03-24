f2cd522727e84b87119ce0c74526da2f716ef356
laouaniFaouzi <faouzi@gmail.com>
2023-03-24 17:30:30 +0100

    Test audio html

--- js/mySourate.js
+++ js/mySourate.js
@@ -1,11 +1,4 @@
-fetch('https://mawaqit.net/fr/mosquee-assalam-de-tomblaine-tomblaine')
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(data, 'text/html');
-
-    // Accédez aux éléments du document HTML récupéré ici
-    const title = htmlDocument.querySelector('.time div');
-    console.log(title.textContent);
-  })
-  .catch(error => console.error(error));
+function playAudio() {
+  var audio = document.getElementById("audio-sourate");
+  audio.play();
+}
