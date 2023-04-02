7101f9f8f91a0a895f36d181017a920ff21534e4
laouaniFaouzi <76941273+laouaniFaouzi@users.noreply.github.com>
2023-04-03 01:13:32 +0200

    MAJ de minuit

--- js/profileName.js
+++ js/profileName.js
@@ -1,30 +1,45 @@
 // Vérifier si le nom de l'utilisateur a déjà été enregistré
 if (!localStorage.getItem('userName')) {
-  // Sélectionner l'élément HTML de la popup et l'afficher
-  const popup = document.querySelector('.popup');
-  popup.style.display = 'flex';
-
-  // Sélectionner les éléments HTML du formulaire de la popup
-  const form = document.querySelector('#popupForm');
-  const nameInput = document.querySelector('#nameInput');
-
-  // Ajouter un écouteur d'événement sur la soumission du formulaire
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    // Récupérer la valeur du champ de saisie
-    const name = nameInput.value;
-
-    // Enregistrer le nom de l'utilisateur dans le stockage local
-    localStorage.setItem('userName', name);
-
-    // Cacher la popup et mettre à jour le nom dans l'élément HTML approprié
-    popup.style.display = 'none';
+    // Sélectionner l'élément HTML de la popup et l'afficher
+    const popup = document.querySelector('.popup');
+    popup.style.display = 'flex';
+  
+    // Sélectionner les éléments HTML du formulaire de la popup
+    const form = document.querySelector('#popupForm');
+    const nameInput = document.querySelector('#nameInput');
+  
+    // Ajouter un écouteur d'événement sur la soumission du formulaire
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      // Récupérer la valeur du champ de saisie
+      const name = nameInput.value;
+  
+      // Enregistrer le nom de l'utilisateur dans le stockage local
+      localStorage.setItem('userName', name);
+  
+      // Cacher la popup et mettre à jour le nom dans l'élément HTML approprié
+      popup.style.display = 'none';
+      const profileName = document.querySelector('#profileName');
+      profileName.textContent = name;
+    });
+  } else {
+    // Si le nom de l'utilisateur a déjà été enregistré, mettre à jour l'élément HTML approprié
     const profileName = document.querySelector('#profileName');
-    profileName.textContent = name;
+    profileName.textContent = localStorage.getItem('userName');
+  }
+  
+  // Sélectionner les éléments HTML de la popup
+  const popupContainer = document.querySelector('.popup');
+  const popupButton = document.querySelector('.popup-button');
+  
+  // Gérer le clic sur le bouton de la popup
+  popupButton.addEventListener('click', function() {
+    var name = document.getElementById('nameInput').value;
+    if(name !== '') {
+      localStorage.setItem('popupShown', 'true');
+      document.getElementById('profileName').innerHTML = name;
+      popup.style.display = 'none';
+    }
   });
-} else {
-  // Si le nom de l'utilisateur a déjà été enregistré, mettre à jour l'élément HTML approprié
-  const profileName = document.querySelector('#profileName');
-  profileName.textContent = localStorage.getItem('userName');
-}
+  
