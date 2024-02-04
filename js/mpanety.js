93f582a03bc304adb6ed6fc42830f64fc0e87e38
FaouziLaouani <LAOUANIFAOUZI@GMAIL.COM>
2024-02-04 15:47:23 +0100

    Upgrade

--- js/mpanety.js
+++ js/mpanety.js
@@ -22,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
-            // Retire la classe 'show' de l'élément offcanvas
             offcanvas.classList.remove('show');
 
             body.style.overflow = '';
@@ -34,11 +33,85 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 window.onload = function() {
-    // Calcul de la hauteur de l'écran moins la taille du header
     const headerHeight = document.querySelector('header').offsetHeight;
     const home = document.getElementById('home');
     const newHomeHeight = window.innerHeight - headerHeight;
     
-    // Appliquer la nouvelle hauteur au contenu
     home.style.height = `${newHomeHeight}px`;
 };
+
+document.addEventListener('click', function (e) {
+    // Vérifier si l'élément cliqué est une ancre
+    if (e.target.tagName === 'A' && e.target.getAttribute('href').startsWith('#')) {
+        e.preventDefault(); // Empêcher le comportement par défaut de l'ancre
+
+        // Récupérer l'élément ciblé par l'ancre
+        var targetElement = document.querySelector(e.target.getAttribute('href'));
+
+        if (targetElement) {
+            // Calculer la hauteur du header
+            var headerHeight = document.querySelector('header').offsetHeight;
+
+            // Faire défiler jusqu'à l'élément ciblé en prenant en compte la hauteur du header
+            window.scrollTo({
+                top: targetElement.offsetTop - headerHeight,
+                behavior: 'smooth' // Ajouter une animation de défilement
+            });
+        }
+    }
+});
+
+// Fonction pour déterminer le jour actuel
+function getDayOfWeek() {
+    var days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+    var today = new Date().getDay();
+    return days[today];
+}
+
+// Fonction pour afficher l'onglet et le bouton du jour actuel
+function showTodayTab() {
+    var today = getDayOfWeek();
+    var tab = document.getElementById(today);
+    var button = document.querySelector('button[onclick="showTab(\'' + today + '\')"]');
+
+    if (tab) {
+        // Ajouter la classe "today" à l'onglet du jour actuel
+        tab.classList.add('today');
+    }
+
+    if (button) {
+        // Ajouter la classe "today" au bouton du jour actuel
+        button.classList.add('today');
+    }
+}
+
+// Fonction pour afficher un onglet
+function showTab(jour) {
+    // Cacher tous les onglets
+    var tabs = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+
+    // Désélectionner tous les boutons
+    var buttons = document.querySelectorAll('#tabs button');
+    for (var j = 0; j < buttons.length; j++) {
+        buttons[j].classList.remove('active');
+    }
+
+    // Afficher l'onglet correspondant au jour
+    var tab = document.getElementById(jour);
+    if (tab) {
+        tab.style.display = 'block';
+    }
+
+    // Sélectionner le bouton correspondant et ajouter la classe "active"
+    var button = document.querySelector('button[onclick="showTab(\'' + jour + '\')"]');
+    if (button) {
+        button.classList.add('active');
+        tab.classList.add('active')
+    }
+}
+
+// Appeler la fonction pour afficher l'onglet et le bouton du jour actuel
+showTodayTab();
